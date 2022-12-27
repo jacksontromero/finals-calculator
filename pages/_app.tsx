576b3dc479a9213f7cc032bd1b3ef1c80aba103a
Jackson Romero <jacksontromero@gmail.com>
2022-12-27 02:08:19 -0500

    Add theme import from json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
+import themeJSON from '../styles/theme.json'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme = createTheme(themeJSON as any);
+
   return (
     <div>
       <link rel="icon" type="image/svg" href="/test.svg"></link>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </div>
   )
 }
