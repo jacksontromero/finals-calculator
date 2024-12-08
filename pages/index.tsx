9b2188b96ef41fb609fcbe81ac5744a3583bdc2a
Jackson Romero <jacksontromero@gmail.com>
2024-12-07 21:57:53 -0500

    remove default thing I accidentally committed

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,109 +54,109 @@ export const defaultBucket: bucket = {
 }
 
 export default function Home() {
-  const [data, setData] = useState<globalData>({
-    classes: [
-      {
-        name: "Probability",
-        number: "36-218",
-        id: uuidv4(),
-        weights: [
-          {
-            name: "Homework",
-            percentage: 35,
-            drops: 2,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Participation",
-            percentage: 5,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Quizzes",
-            percentage: 15,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Midterm",
-            percentage: 20,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Final",
-            percentage: 25,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-        ],
-        selectedAssignment: defaultAssignment,
-        selectedBucket: defaultBucket,
-        targetGrade: 90
-      },
-      {
-        name: "Software",
-        number: "17-214",
-        id: uuidv4(),
-        weights: [
-          {
-            name: "Homework",
-            percentage: 50,
-            drops: 2,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Midterm 1",
-            percentage: 10,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Midterm 2",
-            percentage: 10,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Quizzes",
-            percentage: 5,
-            drops: 4,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Participation",
-            percentage: 5,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-          {
-            name: "Final",
-            percentage: 20,
-            drops: 0,
-            id: uuidv4(),
-            assignments: []
-          },
-        ],
-        selectedAssignment: defaultAssignment,
-        selectedBucket: defaultBucket,
-        targetGrade: 90
-      }
-    ]
-  })
+  // const [data, setData] = useState<globalData>({
+  //   classes: [
+  //     {
+  //       name: "Probability",
+  //       number: "36-218",
+  //       id: uuidv4(),
+  //       weights: [
+  //         {
+  //           name: "Homework",
+  //           percentage: 35,
+  //           drops: 2,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Participation",
+  //           percentage: 5,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Quizzes",
+  //           percentage: 15,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Midterm",
+  //           percentage: 20,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Final",
+  //           percentage: 25,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //       ],
+  //       selectedAssignment: defaultAssignment,
+  //       selectedBucket: defaultBucket,
+  //       targetGrade: 90
+  //     },
+  //     {
+  //       name: "Software",
+  //       number: "17-214",
+  //       id: uuidv4(),
+  //       weights: [
+  //         {
+  //           name: "Homework",
+  //           percentage: 50,
+  //           drops: 2,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Midterm 1",
+  //           percentage: 10,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Midterm 2",
+  //           percentage: 10,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Quizzes",
+  //           percentage: 5,
+  //           drops: 4,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Participation",
+  //           percentage: 5,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //         {
+  //           name: "Final",
+  //           percentage: 20,
+  //           drops: 0,
+  //           id: uuidv4(),
+  //           assignments: []
+  //         },
+  //       ],
+  //       selectedAssignment: defaultAssignment,
+  //       selectedBucket: defaultBucket,
+  //       targetGrade: 90
+  //     }
+  //   ]
+  // })
 
-  // const [data, setData] = useState<globalData>({classes: []})
+  const [data, setData] = useState<globalData>({ classes: [] })
   const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
@@ -176,13 +176,13 @@ export default function Home() {
   const [selectedClassID, setSelectedClassID] = useState(null);
 
   return (
-    <Box sx={{minHeight: "100%"}}>
+    <Box sx={{ minHeight: "100%" }}>
       <Head>
         <title>Final Grade Calculator</title>
         <meta name="description" content="Calculator for what grade you need on an assignment to get a specific grade in a class.  Supports multiple classes, grading schemes that make use of buckets/weights for different types of assignments, dropping/drops for the lowest assignments per bucket, and simulating average performance on assignments." />
       </Head>
-      <Box sx={{minHeight: "100%"}}>
-        <Stack sx={{mt: 2, mx: 2}} direction="column" justifyContent="space-between" spacing={2}>
+      <Box sx={{ minHeight: "100%" }}>
+        <Stack sx={{ mt: 2, mx: 2 }} direction="column" justifyContent="space-between" spacing={2}>
           {
             selectedClassID != null && <ClassDetails data={data} setData={setData} selectedClassID={selectedClassID}></ClassDetails>
           }
